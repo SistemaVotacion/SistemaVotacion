48f9c740fb53b2a30022e56450af2d6fb5c9dfd3
alosslessdev <ppaanncchhoo507@gmail.com>
2024-11-19 12:26:06 -0500

    Update WeatherForecastController.cs

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -12,7 +12,7 @@ using System.Threading.Tasks;
 /*
  * Carrasco, Nathan
  * Herrera, Francisco
- * Wu, Iv·n
+ * Wu, Iv√°n
  */
 
 namespace login.Controllers
@@ -21,17 +21,13 @@ namespace login.Controllers
     [Route("[controller]")]
     public class LoginController : ControllerBase
     {
-        private static readonly List<User> Users = new()
-        {
-            new User { Username = "user1", Password = "password1" },
-            new User { Username = "user2", Password = "password2" }
-        };
+        
 
         [HttpPost]
         public IActionResult Login([FromBody] LoginRequest request)
         {
-            var user = Users.FirstOrDefault(u => u.Username == request.Username && u.Password == request.Password);
-            if (user != null)
+    
+            if (ObtenerDatos != null)
             {
                 return Ok(new { message = "Login successful!" });
             }
@@ -56,27 +52,20 @@ namespace login.Controllers
 
            
 
-            // MÈtodo para obtener todas las reservas existentes
-            internal DataSet ObtenerDatos(int tipo)
+            // M√©todo para obtener todas las reservas existentes
+            internal DataSet ObtenerDatos()
             {
                 DataSet datos = new DataSet();
                 try
                 {
                     using (SqlConnection conexion = new SqlConnection(detallesConexion))
                     {
-                        switch (tipo)
-                        {
-
-                            case 0:
+                        
                                 string query = "SELECT id_reserva, nombre, habitacion, fecha_entrada, fecha_salida," +
                                     " monto_total FROM [Hotel_otaku].[dbo].[Reservas]";
                                 SqlDataAdapter adaptador = new SqlDataAdapter(query, conexion);
                                 adaptador.Fill(datos, "Reservas");
-                                break;
-                            default:
-                                break;
-
-                        }
+                            
                     }
                 }
                 catch (SqlException ex)
