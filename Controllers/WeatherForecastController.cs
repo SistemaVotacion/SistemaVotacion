6db9db74f6719f4615b4841327a83793b335fdc8
alosslessdev <ppaanncchhoo507@gmail.com>
2024-11-19 12:13:37 -0500

    viendo datadapter

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -54,33 +54,7 @@ namespace login.Controllers
         {
             private string detallesConexion = "Data Source=localhost;Initial Catalog=Hotel_otaku;Integrated Security=True";
 
-            // Método para crear una nueva reserva
-            public void CrearReserva(string nombre, string tipoHabitacion, DateTime fechaEntrada, DateTime fechaSalida)
-            {
-                try
-                {
-                    using (SqlConnection connection = new SqlConnection(detallesConexion))
-                    {
-                        SqlCommand command = new SqlCommand("CrearReserva", connection);
-                        command.CommandType = CommandType.StoredProcedure;
-
-                        // Agregar los parámetros del procedimiento
-                        command.Parameters.AddWithValue("@nombre", nombre);
-                        command.Parameters.AddWithValue("@tipo_habitacion", tipoHabitacion);
-                        command.Parameters.AddWithValue("@fecha_entrada", fechaEntrada);
-                        command.Parameters.AddWithValue("@fecha_salida", fechaSalida);
-
-
-                        // Abrir la conexión y ejecutar el procedimiento almacenado
-                        connection.Open();
-                        command.ExecuteNonQuery();
-
-                    }
-                }
-                catch (Exception ex)
-                {
-                }
-            }
+           
 
             // Método para obtener todas las reservas existentes
             internal DataSet ObtenerDatos(int tipo)
@@ -97,24 +71,6 @@ namespace login.Controllers
                                 string query = "SELECT id_reserva, nombre, habitacion, fecha_entrada, fecha_salida," +
                                     " monto_total FROM [Hotel_otaku].[dbo].[Reservas]";
                                 SqlDataAdapter adaptador = new SqlDataAdapter(query, conexion);
-                                adaptador.Fill(datos, "Reservas");
-                                break;
-                            case 1:
-                                query = "SELECT id_reserva, nombre, habitacion, fecha_entrada, fecha_salida," +
-                                  " monto_total FROM [Hotel_otaku].[dbo].[Reservas] WHERE Habitacion = 'Suite'";
-                                adaptador = new SqlDataAdapter(query, conexion);
-                                adaptador.Fill(datos, "Reservas");
-                                break;
-                            case 2:
-                                query = "SELECT id_reserva, nombre, habitacion, fecha_entrada, fecha_salida," +
-                                  " monto_total FROM [Hotel_otaku].[dbo].[Reservas] WHERE Habitacion = 'Doble'";
-                                adaptador = new SqlDataAdapter(query, conexion);
-                                adaptador.Fill(datos, "Reservas");
-                                break;
-                            case 3:
-                                query = "SELECT id_reserva, nombre, habitacion, fecha_entrada, fecha_salida," +
-                                  " monto_total FROM [Hotel_otaku].[dbo].[Reservas] WHERE Habitacion = 'Individual'";
-                                adaptador = new SqlDataAdapter(query, conexion);
                                 adaptador.Fill(datos, "Reservas");
                                 break;
                             default:
