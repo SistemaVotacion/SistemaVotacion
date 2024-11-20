fcbc9404bd7613249bfb57bfec108aa70808fa73
alosslessdev <ppaanncchhoo507@gmail.com>
2024-11-20 04:39:16 -0500

    Update WeatherForecastController.cs

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -56,7 +56,7 @@ namespace login.Controllers
 
             if (user != null)
             {
-                return Ok(new { message = "https://localhost/"}); //aqui poner URL:  "https://localhost/ + provincia + "+" + hashEnBase64"
+                return Ok(new { message = "https://localhost/"}); //aqui poner URL:  "https://localhost/ + provincia + "-" + hashEnBase64"
             } 
             return Unauthorized(new { message = "Invalid username or password." });
         }
