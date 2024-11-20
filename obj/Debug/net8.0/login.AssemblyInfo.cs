68b20b1122c63b0800e9b26af55b42f1da4cb5e6
Shiro2550 <shiro2550@gmail.com>
2024-11-20 10:22:10 -0500

    Home Completo (creo)

--- obj/Debug/net8.0/login.AssemblyInfo.cs
+++ obj/Debug/net8.0/login.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("login")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fe16bf6444487f5b825fc3455afeb51719d4f6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11c63426cfd95680b1377e427fdfe1cf803fbdce")]
 [assembly: System.Reflection.AssemblyProductAttribute("login")]
 [assembly: System.Reflection.AssemblyTitleAttribute("login")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
