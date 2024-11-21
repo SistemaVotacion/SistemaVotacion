67a88a10156f13228b88dd1c11498c668a71e3c9
alosslessdev <ppaanncchhoo507@gmail.com>
2024-11-20 22:58:32 -0500

    Update WeatherForecastController.cs

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -24,11 +24,19 @@ using System.Text.Json;
 //falta mientras la votación esta en progreso el número de votos por candidato
 //podría ser 0 si la base de datos no se puede acceder por estar bloqueada durante la votación 
 
-//tuplas en listas en c#
-//confiar en certificado base de datos
-// bytes en hexadecimal en string en vez de base64 para simplicidad 
-//mover archivos en servidor para renombrar papeletas y darlas
+//(despues)tuplas en listas en c# en API login
+//(después)confiar en certificado base de datos API login
+// (despues)bytes en hexadecimal en string en vez de base64 para simplicidad 
+//(despues)mover archivos en servidor para renombrar papeletas y darlas por el API
 // API para registrar el voto
+// API para estadisticas
+// arreglar html
+// separar css en global y local
+// hacer javascript de login, papeleta
+// hacer variante para desarrollo 5 que solo valide la cédula pero no la revisa con bd
+// hacer trabajo escrito
+// hacer Canva
+// hacer wireframes
 namespace login.Controllers
 {
     [ApiController]
