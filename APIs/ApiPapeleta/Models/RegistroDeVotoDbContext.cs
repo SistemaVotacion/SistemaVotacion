da92edda1824c86e0d028a8e1e00d621853d3b7a
Shiro2550 <shiro2550@gmail.com>
2024-11-23 18:37:39 -0500

    ver api

---
+++ APIs/ApiPapeleta/Models/RegistroDeVotoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace ApiPapeleta.Models
+{
+
+
+        public class RegistroDeVotoDbContext : DbContext
+        {
+            public RegistroDeVotoDbContext(DbContextOptions<RegistroDeVotoDbContext> options) : base(options) { }
+
+            public DbSet<Candidato> Candidatos { get; set; }
+            public DbSet<Provincia> Provincias { get; set; }
+            public DbSet<Voto> Votos { get; set; }
+        }
+    
+}
--- APIs/ApiPapeleta/Program.cs
+++ APIs/ApiPapeleta/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using ApiPapeleta.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,6 +17,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+
 
 app.UseHttpsRedirection();
 
