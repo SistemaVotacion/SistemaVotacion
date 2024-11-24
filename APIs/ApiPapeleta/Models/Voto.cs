a6fc0f9fe63675f952079ddeefbcc61b145c3a3d
Shiro2550 <shiro2550@gmail.com>
2024-11-23 19:08:15 -0500

    Revert "Reapply "ver api""
    This reverts commit fcda2b91dc4d0189073a152b6d8a2f82b536eef5.

--- APIs/ApiPapeleta/Models/Voto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ApiPapeleta.Models
-{
-    public class Voto
-    {
-        public int VotoId { get; set; }
-        public int? CandidatoId { get; set; }
-        public string? Provincia { get; set; }
-    }
-}
