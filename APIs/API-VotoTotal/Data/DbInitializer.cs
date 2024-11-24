c3524ccc8af5f6ea60af38aa7e86079819999edb
alosslessdev <ppaanncchhoo507@gmail.com>
2024-11-23 23:14:35 -0500

    creando api para estadisticas

---
+++ APIs/API-VotoTotal/Data/DbInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using API_VotoTotal.Models;
+
+
+namespace API_VotoTotal.Data { 
+
+    public static class DbInitializer
+    {
+        public static void Initialize(VotoTotalContext context)
+        {
+            // Look for any students.
+            if (context.Candidatos.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+        }
+    }
+}
---
+++ APIs/API-VotoTotal/Models/Provincia.cs
@@ -0,0 +1,11 @@
+﻿namespace API_VotoTotal.Models
+{
+    public class Provincia
+    {
+        public string ID { get; set; }
+
+        public string NombreProvincia { get; set; }
+        public int VotosProvincia { get; set; }
+
+    }
+}
