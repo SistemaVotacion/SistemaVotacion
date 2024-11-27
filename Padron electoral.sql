c118fc7ddf0800f5fa73a686a5889459684ea384
alosslessdev <ppaanncchhoo507@gmail.com>
2024-11-26 22:03:14 -0500

    Update Padron electoral.sql

--- Padron electoral.sql
+++ Padron electoral.sql
@@ -7,19 +7,19 @@ nombre VARCHAR(25) NOT NULL
 
 INSERT INTO provincias (id_provincia, nombre) VALUES
 (1, 'Bocas del Toro'),
-(2, 'CoclÈ'),
-(3, 'ColÛn'),
-(4, 'ChiriquÌ'),
-(5, 'DariÈn'),
+(2, 'Cocl√©'),
+(3, 'Col√≥n'),
+(4, 'Chiriqu√≠'),
+(5, 'Dari√©n'),
 (6, 'Herrera'),
 (7, 'Los Santos'),
-(8, 'Panam·'),
+(8, 'Panam√°'),
 (9, 'Veraguas')
 
 CREATE TABLE Padron (
 id_padron INT IDENTITY PRIMARY KEY NOT NULL,
-cedula_hash VARBINARY(64) NOT NULL,
-pin_hash VARBINARY(64) NOT NULL,
+cedula_hash VARBINARY(MAX) NOT NULL,
+pin_hash VARBINARY(MAX) NOT NULL,
 link VARCHAR (255),
 provincia INT NOT NULL,
 FOREIGN KEY (provincia) REFERENCES provincia(id_provincia)
